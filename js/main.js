05cc25146192fa72568a0d7d22ae1b320764bdfb
soyeb <soyebahmed@gmail.com>
2023-07-26 19:11:19 +0530

    header slider

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-alert('test')
+// add spacer for header
+
+const siteHeaderHeight = document.querySelector('.site-header');
+
+const stickyHeaderSpacer = document.querySelector('.stickyHeaderSpacer');
+
+stickyHeaderSpacer.style.height = siteHeaderHeight.clientHeight + 'px';
+
+
+ 
+// when scroll add class fixed to header
+
+function fixedTop() { 
+   
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition >= 100) {
+                
+        siteHeaderHeight.classList.add('site-header--medium');
+        
+
+    }
+    
+    else { siteHeaderHeight.classList.remove('site-header--medium');  }
+
+}
+
+document.addEventListener('scroll', fixedTop);
